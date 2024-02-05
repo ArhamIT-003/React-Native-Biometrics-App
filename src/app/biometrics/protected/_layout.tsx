942cfb9ce39784440f0780d31e4b9c647d33419d
Arham <muhammadarham59@gmail.com>
2024-02-05 16:57:14 +0500

    New changes

--- src/app/biometrics/protected/_layout.tsx
+++ src/app/biometrics/protected/_layout.tsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { Slot } from "expo-router";
 import { authenticateAsync } from "expo-local-authentication";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function ProtectedLayout() {
   const [unlocked, setUnlocked] = useState(false);
 
-  useEffect(() => {
-    const authenticate = async () => {
-      try {
-        const res = await authenticateAsync();
-        console.log(res);
-        if (res.success) {
-          setUnlocked(true);
-        }
-      } catch (error) {
-        console.error("Authentication failed:", error);
+  const authenticate = async () => {
+    try {
+      const res = await authenticateAsync();
+      console.log(res);
+      if (res.success) {
+        setUnlocked(true);
       }
-    };
+    } catch (error) {
+      console.error("Authentication failed:", error);
+    }
+  };
 
+  useEffect(() => {
     authenticate();
   }, []);
 
   if (!unlocked) {
-    return <Text>Authorize yourself to access the private page</Text>;
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text style={{ fontSize: 20, marginBottom: 20 }}>
+          Authorize yourself to access the private page
+        </Text>
+        <FontAwesome5
+          onPress={authenticate}
+          name="hand-middle-finger"
+          size={75}
+          color="black"
+        />
+      </View>
+    );
   }
   return <Slot />;
 }
